c69614f6f591a03636d54bf1098edd63e121f4eb
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-04-16 22:20:08 -0400

    beginning to work on a page displaying multiple products

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-const API_URL = "https://fakestoreapi.com/products/1"
+const API_URL = "https://fakestoreapi.com/products"
+let products; // object stores all products
 
 const main = async () => {
     const data = await fetch(API_URL);
     const res = await data.json();
-    console.log(res);
 
-    const productTitle = document.querySelector(".product-title");
-    const productDesc = document.querySelector(".product-description");
-    const productPrice = document.querySelector(".product-price");
-    const productImg = document.querySelector(".product-img");
-
-    productTitle.textContent = res.title;
-    productDesc.textContent = res.description;
-    productPrice.textContent = "$" + res.price;
-    productImg.setAttribute("src", res.image);
+    products = res;
+    console.log(products);
 };
-
 main();
+
+
+
+
+// const productTitle = document.querySelector(".product-title");
+// const productDesc = document.querySelector(".product-description");
+// const productPrice = document.querySelector(".product-price");
+// const productImg = document.querySelector(".product-img");
+
+// productTitle.textContent = res.title;
+// productDesc.textContent = res.description;
+// productPrice.textContent = "$" + res.price;
+// productImg.setAttribute("src", res.image);
