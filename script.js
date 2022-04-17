c78fb1e223a6b30b67bee2125cb171c98982656b
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-04-16 23:23:30 -0400

    beginning to work with multiple key;value pairs for each product

--- script.js
+++ script.js
@@ -10,14 +10,21 @@ const main = async () => {
     let numberOfProducts = products.length;
 
     for(i = 0; i < numberOfProducts; i++){
-        let cardTitle = document.createElement("p");
+        let cardTitle = document.createElement("h3");
         let prodTitle = products[i].title;
 
-        let placeholder = document.querySelector(".placeholder");
-        placeholder.appendChild(cardTitle);
+        let cardDescription = document.createElement("p");
+        let prodDescription = products[i].description;
 
         cardTitle.textContent = prodTitle;
+        cardDescription.textContent = prodDescription;
         cardTitle.setAttribute("class", "card-title");
+        cardDescription.setAttribute("class", "card-title2");
+
+        let placeholder = document.querySelector(".placeholder");
+       
+        placeholder.appendChild(cardTitle);
+        placeholder.appendChild(cardDescription);
     }
 };
 
