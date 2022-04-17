896d20501d800991d8d0be75e48a67843958c457
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-04-16 22:51:17 -0400

    working on for loop that creates product card for each index

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const API_URL = "https://fakestoreapi.com/products"
-let products; // object stores all products
+let products = [] // creates object to store all products
+let numberOfProducts = products.length;
 
 const main = async () => {
     const data = await fetch(API_URL);
     const res = await data.json();
-
     products = res;
     console.log(products);
 };
-main();
 
+main(); // calls data from fake store api
 
+console.log(products [0]);
 
+for(i = 0; i <= numberOfProducts; i++){
+    const placeholder = document.querySelector(".placeholder");
 
-// const productTitle = document.querySelector(".product-title");
-// const productDesc = document.querySelector(".product-description");
-// const productPrice = document.querySelector(".product-price");
-// const productImg = document.querySelector(".product-img");
+    let prodContainer = document.createElement("div");
+    let prodText = document.createElement("div");
+    let prodTitleHTML = document.createElement("h2");
+    let prodTitle = products[i].title;
+    let prodDescriptionHTML = document.createElement("p");
+    let prodDescription = products[i].description;
+    let prodPriceHTML = document.createElement("p");
+    let prodPrice = products[i].price;
 
-// productTitle.textContent = res.title;
-// productDesc.textContent = res.description;
-// productPrice.textContent = "$" + res.price;
-// productImg.setAttribute("src", res.image);
+    
+    document.body.insertBefore(prodContainer, placeholder);
+    prodContainer.appendChild(prodText);
+    prodText.appendChild(prodTitleHTML, prodDescriptionHTML, prodPriceHTML);
+}
