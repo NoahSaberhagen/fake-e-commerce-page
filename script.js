5f94e1adee3e7e2a22b90ece16ff5dc1e43aa3a9
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-04-16 23:34:53 -0400

    finished with the important details for each product card

--- script.js
+++ script.js
@@ -10,21 +10,34 @@ const main = async () => {
     let numberOfProducts = products.length;
 
     for(i = 0; i < numberOfProducts; i++){
+        let cardImage = document.createElement("img");
+        let prodImage = products[i].image;
+
         let cardTitle = document.createElement("h3");
         let prodTitle = products[i].title;
 
         let cardDescription = document.createElement("p");
         let prodDescription = products[i].description;
 
+        let cardPrice = document.createElement("p");
+        let prodPrice = products[i].price;
+
+        cardImage.setAttribute("src", prodImage);
         cardTitle.textContent = prodTitle;
         cardDescription.textContent = prodDescription;
+        cardPrice.textContent = prodPrice;
+
+        cardImage.setAttribute("class", "card-image")
         cardTitle.setAttribute("class", "card-title");
-        cardDescription.setAttribute("class", "card-title2");
+        cardDescription.setAttribute("class", "card-description");
+        cardPrice.setAttribute("class", "card-price");
 
         let placeholder = document.querySelector(".placeholder");
        
+        placeholder.appendChild(cardImage);
         placeholder.appendChild(cardTitle);
         placeholder.appendChild(cardDescription);
+        placeholder.appendChild(cardPrice);
     }
 };
 
